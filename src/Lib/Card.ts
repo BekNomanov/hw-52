86566ec074159fd0d92d75fec2b0eded984b5cd6
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2023-12-26 15:39:06 +0600

    Сделал класс Card.ts

---
+++ src/Lib/Card.ts
@@ -0,0 +1,11 @@
+class Cards {
+  public rank: string;
+  public suit: string;
+
+  constructor(rank: string, suit: string) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+}
+
+export default Cards;
