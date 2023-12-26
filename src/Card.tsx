bbc1af18dd31d6be7cd0bc8c2f0cbe39389f503a
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2023-12-26 15:21:13 +0600

    Добавил компонет Card.tsx!

---
+++ src/Card.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface  CardProps {
+  rank: string;
+  suit: string;
+}
+
+const Card: React.FC<CardProps> = ({rank, suit}) =>{
+  return (
+    <span className={`card rank-${rank} ${suit}`}>
+      <span className="rank">{rank}</span>
+      <span className="suit">{getSuitSymbol(suit)}</span>
+    </span>
+  );
+};
+
+const getSuitSymbol = (suit: string): string => {
+  switch (suit) {
+    case 'diams':
+      return '♦';
+    case 'hearts':
+      return '♥';
+    case 'clubs':
+      return '♣';
+    case 'spades':
+      return '♠';
+    default:
+      return '';
+  }
+};
+
+export default Card;
